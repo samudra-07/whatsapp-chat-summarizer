a3fdf54fbb45689dd15454f7ec385f3e2107ba10
Samudragupta Barma <121048540+samudra-07@users.noreply.github.com>
2025-02-21 21:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template, jsonify
 import re
 import pandas as pd
 from transformers import pipeline, T5ForConditionalGeneration, T5Tokenizer
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
