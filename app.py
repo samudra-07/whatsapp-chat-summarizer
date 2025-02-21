3a9fb9fee4658d477453db8393744483f1bb544c
Samudragupta Barma <121048540+samudra-07@users.noreply.github.com>
2025-02-21 22:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import pipeline, T5ForConditionalGeneration, T5Tokenizer
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get('PORT', 4000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
